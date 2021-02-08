fb0a233e92af973b7a4f48ccc9153e89e0dac241
IvanG422 <guerriva000@warren.k12.in.us>
2021-02-08 18:59:44 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        Random rand = new Random();
+
+        /*int number = -0;
+
+        while(number < 10){
+            System.out.println(number);
+            number++;
+
+        System.out.println("Program Ended");
+
+        //Create a while loop that counts backwards from 10.
+        int number = 10;
+
+        while(number > 0){
+            System.out.println(number);
+            number--;
+
+        int number = 1;
+
+        while(number > 30){
+
+            System.out.println(number);
+            number += 3;
+
+        int number = rand.nextInt(19) + 1;
+
+        while(number%2 == 0){
+
+            number = rand.nextInt(19) + 1;
+
+
+        //Create a while loop that validates a user's password.
+        String password = "Krusty Krab";
+
+        System.out.println("Please ener your password");
+        String userGuess = read.nextLine();
+
+        while(!password.equals(userGuess)){
+
+            System.out.println("Please enter your password");
+            userGuess = read.nextLine();
+
+        int number = 10;
+
+        do{
+            System.out.println(number);
+            number--;
+        }while(number > 0)
+
+        int number = 1;
+
+        do{
+            System.out.println(number);
+            number += 3;
+        }while(number < 3);
+        int number;
+
+        do{
+            number = rand.nextInt(19) + 1;
+        }while(number == 0);
+
+        System.out.println(number):
+
+        String password = "Friday";
+
+        String userInput;
+
+        do{
+            System.out.println("Please enter your password.");
+            userInput = read.nextLine();
+        }while(userInput.equals(password));
+
+        for(int number = 1; number < 10; number++){
+            System.out.println(number);
+
+        String word = "Programming";
+
+        for(int i = 0; i < word.length(); i+=2){
+            System.out.println(word.charAt(I));
+
+        String word = "Programming";
+
+        for(int i 2; i < 10; i++){
+            System.out.println(i*2);
+
+        for(int i = 10; i > 0; i--){
+        System.out.println(i);
+
+        for(int i = 1; i > 30; i+=3){
+            System.out.println(i*2);*/
+    }
+
+
+    }
+}
